26ceaab20e587953bdd615ccacaea76c579a1f66
gimslab.com <gimslab.com@gmail.com>
2025-11-16 22:53:06 +0900

    Refactor: Pass pages list to PageViewer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -148,7 +148,7 @@ function App() {
       <main className="main">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/pages/:pageFileName" element={<PageViewer onPageUpdate={handlePageUpdate} />} />
+          <Route path="/pages/:pageFileName" element={<PageViewer onPageUpdate={handlePageUpdate} pages={pages} />} />
           <Route path="/login" element={<LoginPage onLogin={() => setIsAuthenticated(true)} />} />
           <Route path="/new-page" element={<PageEditor onPageUpdate={handlePageUpdate} />} />
           <Route path="/edit/:pageName" element={<PageEditor onPageUpdate={handlePageUpdate} />} />
